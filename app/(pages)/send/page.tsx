9e752d0e3dd19a2212592ed911ab6eefdd378900
Malav12339 <malav.kansara205@gmail.com>
2025-09-24 19:55:03 +0530

    minor change in /send

--- app/(pages)/send/page.tsx
+++ app/(pages)/send/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-
+export const dynamic = "force-dynamic";
 import Button from "@/app/components/Button"
 import InputBox from "@/app/components/InputBox"
 import axios, { AxiosError } from "axios"
